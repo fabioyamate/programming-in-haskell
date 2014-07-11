fb2d36248aee7d6b60271a8d91105b95b5a6a564
Fabio Yamate <fabioyamate@gmail.com>
2014-07-11 10:42:23 -0300

    Add another implementation of halve

--- ch04/ex01.hs
+++ ch04/ex01.hs
@@ -1,5 +1,9 @@
 halve xs = (take half xs, drop half xs)
     where half = (length xs) `div` 2
 
+halve' xs = splitAt half xs
+    where half = (length xs) `div` 2
+
 main = do
     putStrLn . show $ halve [1,2,3,4,5,6]
+    putStrLn . show $ halve' [1,2,3,4,5,6]
