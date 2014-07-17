6351802ddcc40967ef42f81db958fb40d6ce88d0
Fabio Yamate <fabioyamate@gmail.com>
2014-07-16 23:31:42 -0300

    Use inner list compreensions with variable visibility

--- ch05/ex05.hs
+++ ch05/ex05.hs
@@ -1,6 +1,3 @@
-main = do
-    print $ a == b
-    where
-        a = [(x, y) | x <- [1,2,3], y <- [4,5,6]]
-        -- I think that I'm cheating on this answer...
-        b = [x | x <- concat [[(y, 4), (y, 5), (y, 6)] | y <- [1,2,3]]]
+-- [(x, y) | x <- [1,2,3], y <- [4,5,6]]
+
+concat [[(x, y) | x <- [1,2,3]] | y <- [4,5,6]]
