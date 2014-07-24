3476bc997e6591047effae1000af2a314f2afd79
Fabio Yamate <fabioyamate@gmail.com>
2014-07-23 22:28:50 -0300

    Refactor merge implementation
    The initial implementation was complicated because I missed the detail
    that both list are already sorted, so once one gets empty, the other can
    be append entirely to the end

--- ch06/ex04.hs
+++ ch06/ex04.hs
@@ -1,20 +1,10 @@
 merge :: Ord a => [a] -> [a] -> [a]
-merge [] [] = []
-merge (x:y:xs) []
-        | x <= y = x : merge [y] xs
-        | otherwise = y : merge [x] xs
-merge [] (x:y:xs)
-        | x <= y = x : merge [y] xs
-        | otherwise = y : merge [x] xs
-merge (x:xs) []
-    | null xs = [x]
-    | otherwise = x : merge [] xs
-merge [] (x:xs)
-    | null xs = [x]
-    | otherwise = x : merge [] xs
+merge [] xs = xs
+merge xs [] = xs
 merge (x:xs) (y:ys)
         | x <= y = x : merge xs (y:ys)
         | otherwise = y : merge (x:xs) ys
 
 main = do
     print $ merge [2,5,6] [1,3,4]
+    print $ merge [1,2,3] [4,5,6]
