581d51c5344f71c5257e1ab2a6f83f8e095dac30
Fabio Yamate <fabioyamate@gmail.com>
2014-07-23 22:30:09 -0300

    Add answer for exercise 5

---
+++ ch06/ex05.hs
@@ -0,0 +1,27 @@
+-- Merge Sort with merge already implemented is trivial to do.
+-- However, one problem that I got stuck was the case of sorting
+-- a list with a single element, it was in infinite recursion since
+-- it always pattern matched the `msort xs` case.
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort (x:[]) = [x]
+msort xs = merge (msort left) (msort right)
+    where (left, right) = halve xs
+
+halve :: [a] -> ([a], [a])
+halve [] = ([], [])
+halve xs = (take half xs, drop half xs)
+    where half = length xs `div` 2
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] xs = xs
+merge xs [] = xs
+merge (x:xs) (y:ys)
+        | x <= y = x : merge xs (y:ys)
+        | otherwise = y : merge (x:xs) ys
+
+main = do
+    print $ msort ([] :: [Int])
+    print $ msort [1]
+    print $ msort [1..100]
