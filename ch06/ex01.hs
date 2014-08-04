de01aec97834f2803341c5850df4d834dc8dd645
Fabio Yamate <fabioyamate@gmail.com>
2014-08-03 23:44:52 -0300

    Add power evaluation

--- ch06/ex01.hs
+++ ch06/ex01.hs
@@ -4,6 +4,18 @@ power a n
         | n < 0 = error "only non-negative number"
         | otherwise = a * power a (n - 1)
 
+-- power 2 3
+-- = { applying power }
+-- 2 * power 2 2
+-- = { applying power }
+-- 2 * 2 * power 2 1
+-- = { applying power }
+-- 2 * 2 * 2 * power 2 0
+-- = { applying power }
+-- 2 * 2 * 2 * 1
+-- = { applying * }
+-- 8
+
 main = do
     print $ 2 `power` 0
     print $ 2 `power` 3
