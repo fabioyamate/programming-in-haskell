68ad0a423b762ec70a17203a30498c446625cf6e
Fabio Yamate <fabioyamate@gmail.com>
2014-08-06 13:09:14 -0300

    Fix type signature

--- ch07/ex03.hs
+++ ch07/ex03.hs
@@ -1,11 +1,12 @@
 map' :: (a -> b) -> [a] -> [b]
 map' f = foldr ((:) . f) []
 
-map' :: (a -> Bool) -> [a] -> [a]
+filter' :: (a -> Bool) -> [a] -> [a]
 filter' f = foldr selector []
     where selector x acc = if f x then x : acc
                            else acc
 
 main = do
-    print $ map' (*2) [1,2,3,4]
-    print $ filter' odd [1,2,3,4]
+    print $ map (*2) input == map' (*2) input
+    print $ filter odd input == filter' odd input
+    where input = [1,2,3,4]
